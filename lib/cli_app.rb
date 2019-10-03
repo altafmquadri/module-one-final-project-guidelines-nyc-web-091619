01c34b3836f1d1b05aa810d2b2f050d13f7fc43f
Altaf Quadri <altafmquadri@gmail.com>
2019-10-03 09:50:33 -0400

    Refactored code to include menu headings, and migrate browse and purchase functionality to the user class

--- lib/cli_app.rb
+++ lib/cli_app.rb
@@ -2,21 +2,9 @@ class CliApp
     MENU_CHOICES = ["Browse Movies", "Exit"].freeze
 
     def run
-        puts "\n" * 3
-        puts "*" *28
-        puts "Welcome to our movie service"
-        puts "*" *28
-        puts "Please enter your username to begin: "
-        
-        inputted_name = gets.chomp
-        @activated_user = User.activating_user(inputted_name)
-    
-        puts "What would you like to do today #{@activated_user}?"
-        
+        heading
+        login
         main_menu
-        
-        
-        
     end
 
     def main_menu
@@ -25,15 +13,10 @@ class CliApp
         MENU_CHOICES.each.with_index{|choices, i| puts "#{i+1}. #{choices}"}
         puts "\n \n \n"
         main_menu_selection = gets.chomp.to_i
-        mms_validator(main_menu_selection)
-    end
-
-
-    def mms_validator(mms)
-        mms.to_i
-        if mms == 1
-            browse_movies
-        elsif mms == 2
+        if main_menu_selection == 1
+            selected = @activated_user.browse_movies  
+            @activated_user.purchase_movie(selected) ? @activated_user.purchase_movie(selected) : main_menu
+        elsif main_menu_selection == 2
                 puts "Goodbye!"
         else
             puts "Please select a command from 1 to 2!"
@@ -41,26 +24,19 @@ class CliApp
         end
     end
 
-    def browse_movies
-        movies = Movie.all.map{|movie|movie.title}
-        prompt = TTY::Prompt.new
-        movie_choice = prompt.select("Choose a Movie", movies << "Exit", filter: true)
-        purchase_movie(movie_choice)
+    def heading
+        puts "\n" * 3
+        puts "*" *28
+        puts "Welcome to our movie service"
+        puts "*" *28
+        puts "Please enter your username to begin: "
     end
 
-    def purchase_movie(selected_movie)
-        selection = selected_movie
-        prompt = TTY::Prompt.new
-        buy = prompt.yes?("Would you like to purchase #{selection}?")
-
-        if buy
-            thing = Ticket.create(user_id: User.find_by(name: @activated_user).id, movie_id: Movie.find_by(title: selection).id)
-            thing.seat_number = thing.id
-            thing.save
-        else
-            movie = browse_movies
-            purchase_movie(movie) 
-        end
+    def login
+        inputted_name = gets.chomp
+        @activated_user = User.activating_user(inputted_name)
+        puts "\n" * 3
+        puts "What would you like to do today #{@activated_user.name}?"
     end
 
     def exit_movie_menu
--- lib/user.rb
+++ lib/user.rb
@@ -2,18 +2,41 @@ class User < ActiveRecord::Base
     has_many :tickets
     has_many :movies, through: :tickets
     
-    def self.activating_user(name)
-        if User.find_by(name: name)
-            puts "Welcome back"
-            name
+    def self.activating_user(u_name)
+       User.find_or_create_by(name: u_name)
+    end  
+
+
+    def browse_movies
+        movies = Movie.all.map{|movie|movie.title}
+        prompt = TTY::Prompt.new
+        movie_choice = prompt.select("Choose a Movie", movies, filter: true)
+        #purchase_movie(movie_choice)
+    end
+
+    def purchase_movie(selected_movie)
+        selection = selected_movie
+        prompt = TTY::Prompt.new
+        buy = prompt.yes?("Would you like to purchase #{selection}?")
+        if buy
+            Ticket.create(user_id: User.find_by(name: self.name).id, movie_id: Movie.find_by(title: selection).id)
+            false
         else
-        User.create(name: name )
-            puts "Welcome #{name}"
-            name
+            false
         end
     end
 
-    
-
-    
 end
+
+
+
+
+
+ # if User.find_by(name: u_name)
+        #     puts "Welcome back"
+        #     self.name
+        # else
+        # User.create(name: u_name )
+        #     puts "Welcome #{u_name}"
+        #     self.name
+        # end
