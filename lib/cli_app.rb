1be5222dbf518c85f6aa6ff55bf040dbc07a3116
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-10-03 12:32:20 -0400

    built cancel ticket functionality

--- lib/cli_app.rb
+++ lib/cli_app.rb
@@ -1,5 +1,5 @@
 class CliApp
-    MENU_CHOICES = ["Browse Movies", "Exit"].freeze
+    MENU_CHOICES = ["Browse Movies", "View Ticket", "Cancel Ticket", "Exit"].freeze
 
     def run
         heading
@@ -17,6 +17,12 @@ class CliApp
             selected = @activated_user.browse_movies  
             @activated_user.purchase_movie(selected) ? @activated_user.purchase_movie(selected) : main_menu
         elsif main_menu_selection == 2
+            @activated_user.view_ticket
+        elsif main_menu_selection == 3
+            selected = @activated_user.cancel_ticket_display
+            @activated_user.cancel_ticket(selected) ? @activated_user.cancel_ticket(selected) : main_menu
+            #binding.pry
+        elsif main_menu_selection == 4
                 puts "Goodbye!"
         else
             puts "Please select a command from 1 to 2!"
--- lib/user.rb
+++ lib/user.rb
@@ -26,6 +26,35 @@ class User < ActiveRecord::Base
         end
     end
 
+    def view_ticket
+        #user does not get tickets passed
+        ##  Ticket.all.find_by(id: 1) #gives me the first user "Jason" with user_id & movie_id 1
+        ##  User.all.find_by(id: 1) # gives me the first user "Jason" with  id (user_id in Tickets) 1
+        tickets = self.tickets.all
+        my_tickets = tickets.map{|ticket| "#{Movie.find_by(id: ticket.movie_id).title}: $#{Movie.find_by(id: ticket.movie_id).price}"}
+        puts my_tickets
+        my_tickets
+    end
+
+    def cancel_ticket_display
+        prompt = TTY::Prompt.new
+        ticket_list = self.view_ticket
+        selected_movie = prompt.select("Choose a Movie", ticket_list, filter: true)
+    end
+
+    def cancel_ticket(selected_movie)
+        selection = selected_movie
+        selection = selection.split[0].gsub(":","")
+        prompt = TTY::Prompt.new
+        cancel = prompt.yes?("Would you like to cancel #{selection}?")
+        if cancel
+            ticket_to_cancel = Ticket.find_by(user_id: self.id, movie_id: Movie.find_by(title: selection).id)
+            ticket_to_cancel.destroy
+            false
+        else
+            false
+        end
+    end
 end
 
 
